99aab8bc05dc5945ccc05ca66d7fdb644b9fe1cf
Symon Lima <symonlima07@gmail.com>
2024-10-30 18:12:36 -0300

    Desafio 4

---
+++ src/Desafio.java
@@ -0,0 +1,58 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Titular: ");
+        String titular = scanner.nextLine(); // Mudei para nextLine() para ler o titular completo
+        System.out.print("Número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        System.out.print("Saldo: ");
+        double saldo = scanner.nextDouble();
+        System.out.print("Taxa de Juros: ");
+        double taxaJuros = scanner.nextDouble();
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+
+        System.out.println("Conta Poupanca:");
+        contaPoupanca.exibirInformacoes();
+        scanner.close();
+    }
+}
+
+class ContaBancaria {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void exibirInformacoes() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println("Titular: " + titular);
+        System.out.println("Número: " + numero);
+        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    private double taxaJuros;
+
+    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+        super(numero, titular, saldo);
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println("Taxa de juros: " + decimalFormat.format(taxaJuros) + "%");
+    }
+}
